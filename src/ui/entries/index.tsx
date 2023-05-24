c778dd4ef639a36f0212863baf83c2d939fd122a
Joshuan <joshuan.chel@gmail.com>
2023-05-24 13:14:39 +0200

    Add uikit and broke client code

--- src/ui/entries/index.tsx
+++ src/ui/entries/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {Button} from '@gravity-ui/uikit';
+import '@gravity-ui/uikit/styles/styles.css';
+import {ThemeProvider} from '@gravity-ui/uikit';
 
 const container = document.getElementById('root');
 
 const root = createRoot(container);
 
-root.render(<h1>Hello from raect</h1>);
+const SubmitButton = <Button view="action" size="l">I am button</Button>;
+
+root.render(
+    <ThemeProvider theme="light">
+        <h1>Hello from raect</h1>
+        <SubmitButton />
+    </ThemeProvider>
+);
